f71d6cadd70002039500f7120c7539ba7912c0cb
GweepCreative <gweepcreative@gmail.com>
2025-03-25 00:30:46 +0300

    new logos

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,74 +1,94 @@
 const data = [
-    {
-      name: "CNN",
-      image: "/logos/cnn.png",
-      alt: "CNN",
-    },
-    {
-      name: "Demirören",
-      image: "/logos/demiroren.png",
-      alt: "Demirören",
-    },
-    {
-      name: "Espresso Lab",
-      image: "/logos/eslab.png",
-      alt: "Espresso Lab",
-    },
-    {
-      name: "Ets Tur",
-      image: "/logos/ets.png",
-      alt: "Ets Tur",
-    },
-    {
-      name: "Iddaa",
-      image: "/logos/iddaa.png",
-      alt: "Iddaa",
-    },
-    {
-      name: "Idefix",
-      image: "/logos/idefix.png",
-      alt: "Idefix",
-    },
-    {
-      name: "İhlas Ev Aletleri",
-      image: "/logos/ihlas.png",
-      alt: "İhlas Ev Aletleri",
-    },
-    {
-      name: "Kilim",
-      image: "/logos/kilim.png",
-      alt: "Kilim",
-    },
-    {
-      name: "Milangaz",
-      image: "/logos/milangaz.png",
-      alt: "Milangaz",
-    },
-    {
-      name: "Misli",
-      image: "/logos/misli.png",
-      alt: "Misli",
-    },
-    {
-      name: "TGRT",
-      image: "/logos/tgrt.png",
-      alt: "TGRT",
-    },
-    {
-      name: "TRT",
-      image: "/logos/trt.svg",
-      alt: "TRT",
-    },
-    {
-      name: "Turkuvaz Kitap",
-      image: "/logos/turkuvaz.png",
-      alt: "Turkuvaz Kitap",
-    },
-    {
-      name: "Ülker",
-      image: "/logos/ulker.png",
-      alt: "Ülker",
-    },
-  ]
+  {
+    name: "CNN",
+    image: "/logos/cnn.png",
+    alt: "CNN",
+  },
+  {
+    name: "Demirören",
+    image: "/logos/demiroren.png",
+    alt: "Demirören",
+  },
+  {
+    name: "D&R",
+    image: "/logos/dnr.png",
+    alt: "D&R",
+  },
+  {
+    name: "Espresso Lab",
+    image: "/logos/eslab.png",
+    alt: "Espresso Lab",
+  },
+  {
+    name: "Ets Tur",
+    image: "/logos/ets.png",
+    alt: "Ets Tur",
+  },
+  {
+    name: "Iddaa",
+    image: "/logos/iddaa.png",
+    alt: "Iddaa",
+  },
+  {
+    name: "Idefix",
+    image: "/logos/idefix.png",
+    alt: "Idefix",
+  },
+  {
+    name: "İhlas Ev Aletleri",
+    image: "/logos/ihlas.png",
+    alt: "İhlas Ev Aletleri",
+  },
+  {
+    name: "İstikbal",
+    image: "/logos/istikbal.png",
+    alt: "İstikbal",
+  },
+  {
+    name: "İstiklal Mall",
+    image: "/logos/istiklalmall.png",
+    alt: "İstiklal Mall",
+  },
+  {
+    name: "Kilim",
+    image: "/logos/kilim.png",
+    alt: "Kilim",
+  },
+  {
+    name: "Milangaz",
+    image: "/logos/milangaz.png",
+    alt: "Milangaz",
+  },
+  {
+    name: "Milli Piyango",
+    image: "/logos/millipiyango.png",
+    alt: "Milli Piyango",
+  },
+  {
+    name: "Misli",
+    image: "/logos/misli.png",
+    alt: "Misli",
+  },
+  {
+    name: "TGRT",
+    image: "/logos/tgrt.png",
+    alt: "TGRT",
+  },
+  {
+    name: "TRT",
+    image: "/logos/trt.svg",
+    alt: "TRT",
+  },
+  {
+    name: "Turk Petrol",
+    image: "/logos/turkpetrol.png",
+    alt: "TurkPetrol",
+  },
+  {
+    name: "Ülker",
+    image: "/logos/ulker.png",
+    alt: "Ülker",
+  },
+]
 
 export default data;
