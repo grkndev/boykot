eebe591d5be94560178742f9f7dfd532f71a9e6c
GweepCreative <gweepcreative@gmail.com>
2025-04-17 20:41:12 +0300

    new domain

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,10 +1,10 @@
 async function getData() {
-  const res = await fetch("https://boykotyap.net/data/list.json");
+  const res = await fetch("https://boykotyap.org/data/list.json");
   const data = await res.json();
   return data.images;
 }
 async function getTitle() {
-  const res = await fetch("https://boykotyap.net/data/hero.json");
+  const res = await fetch("https://boykotyap.org/data/hero.json");
   const data = await res.json();
   return data;
 }
